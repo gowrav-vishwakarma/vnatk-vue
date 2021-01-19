c91a3d98da46fe47d8ba645f2801fad87e3f284b
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2021-01-19 18:06:10 +0530

    added vuetify-form-base as dependency

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -53,6 +53,7 @@ const external = [
   // list external dependencies, exactly the way it is written in the import statement.
   // eg. 'jquery'
   'vue',
+  'vuetify-form-base'
 ];
 
 // UMD/IIFE shared settings: output.globals
