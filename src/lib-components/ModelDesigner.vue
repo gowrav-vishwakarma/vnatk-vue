7ec290794fdda059a6a36190c5a3972e65ffd0b2
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2021-01-24 13:04:47 +0530

    WIP ModelDesigner

---
+++ src/lib-components/ModelDesigner.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-app-bar app> </v-app-bar>
+
+    <v-card height="400" width="256" class="mx-auto">
+      <v-navigation-drawer permanent app>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              VNATL ModelDesigner
+            </v-list-item-title>
+            <v-list-item-subtitle> Real agile </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense nav>
+          <v-list-item v-for="(model, name) in models" :key="name" link>
+            <v-list-item-icon>
+              <v-icon>mdi-cogs</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+    <v-main>
+      <v-container> Hello World </v-container>
+    </v-main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModelDesigner",
+  props: {
+    service: [Object],
+  },
+
+  data() {
+    return {
+      models: {},
+    };
+  },
+
+  mounted() {
+    this.service.service
+      .get(this.service.basepath + "/modeldesigner")
+      .then((response) => {
+        this.models = response.data;
+      });
+  },
+};
+</script>
