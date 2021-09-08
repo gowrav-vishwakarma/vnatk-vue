08b8717602979f033e4b6b8141e7bfa2e41485d1
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2021-09-08 16:31:04 +0530

    Added to pmenu slots

--- dev/services/catalog.js
+++ dev/services/catalog.js
@@ -3,13 +3,15 @@ import axios from "axios";
 //import mock_api from "@/services/mock_customer";
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_BASE_URL_CUSTOMER || "http://localhost:3002"
+  baseURL:
+    process.env.VUE_APP_BASE_URL_CUSTOMER ||
+    "https://catalog.service.staging.frendy.in",
 });
 
-
 api.interceptors.request.use(
-  async config => {
-    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJmcmVuZHkiLCJsYXN0TmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBmcmVuZHkuaW4iLCJ1c2VyVHlwZSI6ImVtcGxveWVlIiwiaWF0IjoxNjEzNTgwNzA2LCJleHAiOjE2NDUxMTY3MDZ9.Wwya4iThBfJX_y3S2Fr4X6p8KE55j1zkQbaJVKltojM";
+  async (config) => {
+    var token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJmcmVuZHkiLCJsYXN0TmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBmcmVuZHkuaW4iLCJ1c2VyVHlwZSI6ImVtcGxveWVlIiwiaWF0IjoxNjEzNTgwNzA2LCJleHAiOjE2NDUxMTY3MDZ9.Wwya4iThBfJX_y3S2Fr4X6p8KE55j1zkQbaJVKltojM";
     if (token) {
       config.headers.Authorization = "Bearer " + token;
     }
@@ -26,7 +28,7 @@ api.interceptors.request.use(
     // }
     return config;
   },
-  error => {
+  (error) => {
     return Promise.reject(error);
   }
 );
