c55ea9fd7ceb15e20cfae13a152ca9c61d4846c3
Rakesh Sinha <rakesh.s@frendy.in>
2021-10-23 18:06:55 +0530

    example added

--- dev/serve.vue
+++ dev/serve.vue
@@ -7,7 +7,7 @@
     <!-- <h1>Example 1</h1>
     <example1 /> -->
     <h1>Example 2</h1>
-    <example2 />
+    <exampleAutoCompleteWhere />
     <!-- <h1>Example 3</h1>
     <example3 /> -->
     <!-- <model-designer-example /> -->
@@ -22,10 +22,11 @@ import Vue from "vue";
 // import Test from "./test.vue";
 // import ExampleExport from "./example-export.vue";
 // import Example1 from "./example1.vue";
-import Example2 from "./example2.vue";
+// import Example2 from "./example2.vue";
 // import Example3 from "./example3.vue";
 // import ExampleFull from "./example-full.vue";
 // import modelDesignerExample from "./modelDeginerExample.vue";
+import exampleAutoCompleteWhere from "./exampleAutoCompleteWhere.vue";
 
 export default Vue.extend({
   name: "ServeDev",
@@ -34,10 +35,11 @@ export default Vue.extend({
     // "example-export": ExampleExport,
     // Test,
     // Example1,
-    Example2,
+    // Example2,
     // Example3,
     // ExampleFull,
     // modelDesignerExample,
+    exampleAutoCompleteWhere,
   },
 });
 </script>
